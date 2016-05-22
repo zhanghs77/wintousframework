cc777fcb82aae322187d0abb79fb4f63c89007e3
zhanghs <zhanghs@zhanghs-PC>
2016-05-22 23:37:51 +0800

    controller中注入service测试

--- wintousframework-web/src/main/java/com/wintous/web/controller/test/TestController.java
+++ wintousframework-web/src/main/java/com/wintous/web/controller/test/TestController.java
@@ -1,20 +1,30 @@
 package com.wintous.web.controller.test;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.wintous.model.test.Student;
 import com.wintous.web.controller.base.BaseController;
+import com.wintousframework.service.StudentService;
 
 @Controller
 @RequestMapping("/test")
 public class TestController extends BaseController {
 
+	@Autowired
+	public StudentService studentService;
+	
 	public TestController(){
 		System.out.println("TestController");
 	}
 	@RequestMapping(value="/index.do",method=RequestMethod.GET)
-	public String doIndex(){
+	public String doIndex(Model map){
+		Student student=studentService.getById(8);
+		System.out.println(student.getAge());
+		map.addAttribute("student",student);
 		return "index";
 	}
 }
--- wintousframework-web/src/main/webapp/WEB-INF/page/index.jsp
+++ wintousframework-web/src/main/webapp/WEB-INF/page/index.jsp
@@ -1,5 +1,19 @@
+<%@ page language="java" import="java.util.*" pageEncoding="UTF-8"%>
 <html>
 <body>
 <h2>Hello World!</h2>
+<table>
+<thead>
+	<tr>
+	<th>名称</th>
+	<th>年龄</th>
+	</tr>
+</thead>
+<tbody>
+<tr>
+	<td>${student.name}</td><td>${student.age}</td>
+</tr>
+</tbody>
+</table>
 </body>
 </html>
